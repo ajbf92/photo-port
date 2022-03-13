4ad3814fc706306a79ae673644278ecd53e931cf
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-03-13 17:39:01 -0400

    added states/hooks and conditional rendering to profile

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,50 +1,56 @@
 import React from "react";
-const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
-function categorySelected(name) {
-    console.log(`${name} clicked`)
-} 
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-    return(
-        <header>
-            <h2>
-                <a data-testid= "link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-                </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a data-testid="about" href="#about">
-                            About me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                    {categories.map((category)=> (
-                        <li className="mx-1"
-                        key={category.name}>
-                            {/* we added "()" to wrap the callback function declaration in a function declaration so it doesnt call  whenever the component is rendered */}
-                            <span onClick={() => categorySelected(category.name)}>{category.name}</span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  }
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
+            </a>
+          </li>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
+              {/* we added "()" to wrap the callback function declaration in a function declaration so it doesn't call  whenever the component is rendered */}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
